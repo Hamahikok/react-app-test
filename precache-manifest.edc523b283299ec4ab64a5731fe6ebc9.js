310fa9fcf048bbec7b664ffcf2838c027ce9a50e
reckchu <reckchu@gmail.com>
2019-10-21 14:14:16 +0800

    Updates

---
+++ precache-manifest.edc523b283299ec4ab64a5731fe6ebc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beddad24b796053fc959fffe8fab322f",
+    "url": "/react-app-test.io/index.html"
+  },
+  {
+    "revision": "ff089c9b6f8772d20056",
+    "url": "/react-app-test.io/static/css/main.21c6e1ed.chunk.css"
+  },
+  {
+    "revision": "96a3823ee64382982162",
+    "url": "/react-app-test.io/static/js/2.bd1da6c0.chunk.js"
+  },
+  {
+    "revision": "ff089c9b6f8772d20056",
+    "url": "/react-app-test.io/static/js/main.c13b88ea.chunk.js"
+  },
+  {
+    "revision": "57a5ddfb8e61e4e9de73",
+    "url": "/react-app-test.io/static/js/runtime-main.37a72645.js"
+  }
+]);
