4b3e7e4cc50425462d188968475c6cfa00bb99bf
reckchu <reckchu@gmail.com>
2019-10-21 14:01:27 +0800

    Updates

---
+++ precache-manifest.27e242a89c866d7cf9fc057a23ba127e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a506f7975d7e78b70c815e8dc2181a4c",
+    "url": "/Hamahikok/react-app-test.io/index.html"
+  },
+  {
+    "revision": "d7ec69cab803878e9253",
+    "url": "/Hamahikok/react-app-test.io/static/css/main.21c6e1ed.chunk.css"
+  },
+  {
+    "revision": "96a3823ee64382982162",
+    "url": "/Hamahikok/react-app-test.io/static/js/2.bd1da6c0.chunk.js"
+  },
+  {
+    "revision": "d7ec69cab803878e9253",
+    "url": "/Hamahikok/react-app-test.io/static/js/main.8d627f2f.chunk.js"
+  },
+  {
+    "revision": "4fbac28965d16942b0c4",
+    "url": "/Hamahikok/react-app-test.io/static/js/runtime-main.cced2d02.js"
+  }
+]);
